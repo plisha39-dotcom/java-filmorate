157ece11d228f22f5b2427c79ae264ab3f92fde4
Rashit-iksanov <iksanovrashit@yandex.ru>
2026-09-08 00:13:39 +0500

    Добавил тест

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -105,8 +105,4 @@ public class InMemoryFilmStorage implements FilmStorage {
     private long getNextId() {
         return ++currentId;
     }
-
-    public void removeLikesByUser(Long userId) {
-        films.values().forEach(film -> film.getLikes().remove(userId));
-    }
 }
