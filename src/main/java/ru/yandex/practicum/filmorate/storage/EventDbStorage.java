8d466f94442e9cc1386f98777b4c1517779c9c62
Darya <daria.koshkina228@yandex.ru>
2026-09-08 17:01:07 +0500

    fix: убрала неиспольз импорт в евент стораже

--- src/main/java/ru/yandex/practicum/filmorate/storage/EventDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventDbStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
