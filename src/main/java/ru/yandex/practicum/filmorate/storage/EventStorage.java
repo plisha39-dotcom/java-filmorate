4c08fae56692191d465913df6e7fb0e460b771eb
Darya <daria.koshkina228@yandex.ru>
2026-09-08 17:19:33 +0500

    fix: добавила отступы

--- src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
@@ -5,6 +5,8 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface EventStorage {
+
     void addEvent(Event event);
+
     List<Event> getFeedByUserId(Long userId);
 }
