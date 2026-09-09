7fece17d4a1e5654befd28472ffe44aaca2d4a8a
Rashit-iksanov <iksanovrashit@yandex.ru>
2026-09-09 20:25:28 +0500

    добавил валидацию существования режиссера при создании/обновлении фильма

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -27,6 +28,6 @@ public class Film {
     private Mpa mpa;
     private Set<Genre> genres = new HashSet<>();
 
-    @JsonProperty("director")
+    @JsonAlias({"director", "directors"})
     private Set<Director> directors = new HashSet<>();
 }
